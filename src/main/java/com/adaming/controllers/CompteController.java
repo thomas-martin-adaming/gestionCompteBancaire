24112af03e8fe379b69453ebf05dfa5f00b4eed5
thomas-martin-adaming <gmartinthomas7@gmail.com>
2020-11-16 16:37:06 +0100

    Vulnerability fixed

--- src/main/java/com/adaming/controllers/CompteController.java
+++ src/main/java/com/adaming/controllers/CompteController.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adaming.entities.Compte;
@@ -20,22 +21,27 @@ public class CompteController {
 	@Autowired
 	CompteService compteService;
 	
-	@RequestMapping(value = "/comptes", method = RequestMethod.GET)
+	
+	//@RequestMapping(value = "/comptes", method = RequestMethod.GET)
+	@GetMapping(value = "/comptes")
 	public List<Compte> findAll() {
 		return compteService.findAll();
 	}
 
-	@RequestMapping(value = "/comptes/{id}", method = RequestMethod.GET)
+	//@RequestMapping(value = "/comptes/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/comptes/{id}")
 	public Optional<Compte> findOne(@PathVariable("id") Long id) {
 		return compteService.findOne(id);
 	}
 
-	@RequestMapping(value = "/comptes", method = RequestMethod.POST)
+	//@RequestMapping(value = "/comptes", method = RequestMethod.POST)
+	@PostMapping(value = "/comptes")
 	public Compte save(@RequestBody Compte compte) {
 		return compteService.save(compte);
 	}
 
-	@RequestMapping(value = "/comptes/{id}", method = RequestMethod.DELETE)
+	//@RequestMapping(value = "/comptes/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/comptes/{id}")
 	public void delete(@PathVariable("id") Long id) {
 		compteService.delete(id);
 	}
--- src/main/java/com/adaming/controllers/UtilisateurController.java
+++ src/main/java/com/adaming/controllers/UtilisateurController.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adaming.entities.Utilisateur;
@@ -19,22 +20,26 @@ public class UtilisateurController {
 	@Autowired
 	UtilisateurService utilisateurService;
 	
-	@RequestMapping(value = "/utilisateurs", method = RequestMethod.GET)
+	//@RequestMapping(value = "/utilisateurs", method = RequestMethod.GET)
+	@GetMapping(value = "/utilisateurs")
 	public List<Utilisateur> findAll() {
 		return utilisateurService.findAll();
 	}
 
-	@RequestMapping(value = "/utilisateurs/{id}", method = RequestMethod.GET)
+	//@RequestMapping(value = "/utilisateurs/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/utilisateurs/{id}")
 	public Optional<Utilisateur> findOne(@PathVariable("id") Long id) {
 		return utilisateurService.findOne(id);
 	}
 
-	@RequestMapping(value = "/utilisateurs", method = RequestMethod.POST)
+	//@RequestMapping(value = "/utilisateurs", method = RequestMethod.POST)
+	@PostMapping(value = "/utilisateurs")
 	public Utilisateur save(@RequestBody Utilisateur utilisateur) {
 		return utilisateurService.save(utilisateur);
 	}
 
-	@RequestMapping(value = "/utilisateurs/{id}", method = RequestMethod.DELETE)
+	//@RequestMapping(value = "/utilisateurs/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/utilisateurs/{id}")
 	public void delete(@PathVariable("id") Long id) {
 		utilisateurService.delete(id);
 	}
